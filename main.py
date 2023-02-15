3d38ce7eaa36cc40216913d862ece6d5aef694f3
aaronlee <aaronlee.uva@gmail.com>
2023-02-14 19:49:10 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import csv
+import json
+import requests
+
+url = 'https://rest.iad-06.braze.com/users/track'
+API_KEY = 'Bearer a7648135-f31a-44bf-a6eb-2dbf9de8b8d6'
+
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': API_KEY
+}
+
+with open('event_upload.csv') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        if len(row) == 0:
+            continue
+        temp = row[0].split(",")
+        if temp[0] == "external_id":
+            continue
+
+        body = json.dumps({
+            "events":[{
+                "external_id": temp[0],
+                "name": "feb2022_challenge_signup",
+                "time": temp[1]}
+            ]})
+
+        response = requests.post(url, data=body, headers=headers)
+        print(response.json())
